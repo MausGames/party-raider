69de6f145ad7d7ce5053a31e694455357bee023c
Martin Mauersics <revision@maus-games.at>
2020-09-22 22:04:02 +0200

    updated license format

--- source/main.h
+++ source/main.h
@@ -1,39 +1,28 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-//*---------------------------------------------------------------------------------------*//
-//|   _____        _____ _________     __   _____            _____ _____  ______ _____    |//
-//|  |  __ \ /\   |  __ \__   __\ \   / /  |  __ \     /\   |_   _|  __ \|  ____|  __ \   |//
-//|  | |__) /  \  | |__) | | |   \ \_/ /   | |__) |   /  \    | | | |  | | |__  | |__) |  |//
-//|  |  ___/ /\ \ |  _  /  | |    \   /    |  _  /   / /\ \   | | | |  | |  __| |  _  /   |//
-//|  | |  / ____ \| | \ \  | |     | |     | | \ \  / ____ \ _| |_| |__| | |____| | \ \   |//
-//|  |_| /_/    \_\_|  \_\ |_|     |_|     |_|  \_\/_/    \_\_____|_____/|______|_|  \_\  |//
-//|                                                                                       |//
-//*---------------------------------------------------------------------------------------*//
-/////////////////////////////////////////////////////////////////////////////////////////////
-//*---------------------------------------------------------------------------------------*//
-//| Party Raider v1.0 (https://www.maus-games.at)                                         |//
-//*---------------------------------------------------------------------------------------*//
-//| Copyright (c) 2019 Martin Mauersics                                                   |//
-//|                                                                                       |//
-//| This software is provided 'as-is', without any express or implied                     |//
-//| warranty. In no event will the authors be held liable for any damages                 |//
-//| arising from the use of this software.                                                |//
-//|                                                                                       |//
-//| Permission is granted to anyone to use this software for any purpose,                 |//
-//| including commercial applications, and to alter it and redistribute it                |//
-//| freely, subject to the following restrictions:                                        |//
-//|                                                                                       |//
-//|   1. The origin of this software must not be misrepresented; you must not             |//
-//|   claim that you wrote the original software. If you use this software                |//
-//|   in a product, an acknowledgment in the product documentation would be               |//
-//|   appreciated but is not required.                                                    |//
-//|                                                                                       |//
-//|   2. Altered source versions must be plainly marked as such, and must not be          |//
-//|   misrepresented as being the original software.                                      |//
-//|                                                                                       |//
-//|   3. This notice may not be removed or altered from any source                        |//
-//|   distribution.                                                                       |//
-//*---------------------------------------------------------------------------------------*//
-/////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
+//*----------------------------------------------------------------------------*//
+//| Party Raider v1.0.0 (https://www.maus-games.at)                            |//
+//*----------------------------------------------------------------------------*//
+//| Copyright (c) 2019 Martin Mauersics                                        |//
+//|                                                                            |//
+//| This software is provided 'as-is', without any express or implied          |//
+//| warranty. In no event will the authors be held liable for any damages      |//
+//| arising from the use of this software.                                     |//
+//|                                                                            |//
+//| Permission is granted to anyone to use this software for any purpose,      |//
+//| including commercial applications, and to alter it and redistribute it     |//
+//| freely, subject to the following restrictions:                             |//
+//|                                                                            |//
+//| 1. The origin of this software must not be misrepresented; you must not    |//
+//|    claim that you wrote the original software. If you use this software    |//
+//|    in a product, an acknowledgment in the product documentation would be   |//
+//|    appreciated but is not required.                                        |//
+//|                                                                            |//
+//| 2. Altered source versions must be plainly marked as such, and must not be |//
+//|    misrepresented as being the original software.                          |//
+//|                                                                            |//
+//| 3. This notice may not be removed or altered from any source distribution. |//
+//*----------------------------------------------------------------------------*//
+//////////////////////////////////////////////////////////////////////////////////
 #pragma once
 #ifndef _PAR_GUARD_MAIN_H_
 #define _PAR_GUARD_MAIN_H_
